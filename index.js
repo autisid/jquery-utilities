14e3014fe71c6de93865f87c4dddd149cd59b716
Sidiq Fahmi <sidiq@email.com>
2024-03-11 23:09:22 +0700

    feat : init commit - add index.js - add package.json - add gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+//app namespace - common function
+var app = app || {};
+ 
+// app page specific namespace
+app.pg = app.pg || {};
+
+// app common function
+(function(o){
+    o.init = function(){
+    }
+
+}(app));
+
+// initialize  app
+
+$(function(){
+
+    app.init();
+
+});
